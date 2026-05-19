6247928d7a8a0ee85f669c0f81470fb8614aa25f
AndrewBlack21 <andrew.moraes16@hotmail.com>
2026-05-19 02:10:03 -0300

    "Feat/clients/auth"

--- src/services/supabase/clients.ts
+++ src/services/supabase/clients.ts
@@ -1,2 +1,23 @@
 import { createClient } from "@supabase/supabase-js";
 import * as SecureStore from "expo-secure-store";
+import { Platform } from "react-native";
+
+const URL = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => SecureStore.getItemAsync(key),
+  setItem: (key: string, value: string) => SecureStore.setItemAsync(key, value),
+  removeItem: (key: string) => SecureStore.deleteItemAsync(key),
+};
+
+const storage = Platform.OS === "web" ? localStorage : ExpoSecureStoreAdapter;
+
+export const supabase = createCliente(URL, KEY, {
+  auth: {
+    storage,
+    autoRefreshToken: true,
+    persisSession: true,
+    detectSessionInUrl: Platform.OS === "web",
+  },
+});
